b5c13bf372ffe5b646846a1dfdc275dae0922d1a
Sittichai Choosumrong <sittichai.ocu@gmail.com>
2019-06-27 22:01:05 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -36,11 +36,11 @@
     }
   #ตัวอย่าง Message Type "Image"
     else if($message == "รูปบอมป์"){
-        $image_url = "https://previews.123rf.com/images/izmask/izmask1203/izmask120300101/12928853-vector-of-icon-for-price-bomp-place-for-customer-text.jpg";
+        $image_url1 = "https://previews.123rf.com/images/izmask/izmask1203/izmask120300101/12928853-vector-of-icon-for-price-bomp-place-for-customer-text.jpg";
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "image";
-        $arrayPostData['messages'][0]['originalContentUrl'] = $image_url;
-        $arrayPostData['messages'][0]['previewImageUrl'] = $image_url;
+        $arrayPostData['messages'][0]['originalContentUrl'] = $image_url1;
+        $arrayPostData['messages'][0]['previewImageUrl'] = $image_url1;
         replyMsg($arrayHeader,$arrayPostData);
     }
     #ตัวอย่าง Message Type "Location"
